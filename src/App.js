3cdccdddce8bd5f98708537752be78bf21b1c13c
Petr Šťastný <petr.stastny99@gmail.com>
2023-09-30 13:26:34 +0200

    dalsi testovani/uceni

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react"
 
+import Book from "./components/Book"
+
 const App = () => {
+    const [show, setShow] = useState(false)
 
     return (
         <div>
-            <h1>Obsah stranky</h1>         
+            <button type="button" onClick={ () => show ? setShow(false) : setShow(true)} >Ukaz / skryj</button>
+
+            {show && <Book />}
         </div>
     )
 
---
+++ src/components/Book.js
@@ -0,0 +1,9 @@
+import "./Book.css"
+
+const Book = () => {
+    return (
+        <h1>Kniha </h1>
+    )
+}
+
+export default Book
